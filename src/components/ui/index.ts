e20b3d4a2be26a1a437d2d436cabfc05a2f09903
Jorge Rivera Mancilla <jiriveramancilla@gmail.com>
2025-09-23 22:17:37 -0300

    feat(components): update component barrel files

---
+++ src/components/ui/index.ts
@@ -0,0 +1,20 @@
+export { Button } from "./button";
+export {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./carousel";
+export { Input } from "./input";
+export { Label } from "./label";
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./pagination";
+export { Skeleton } from "./skeleton";
