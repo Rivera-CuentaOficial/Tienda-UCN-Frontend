de981830128b6ee966878e67d71c8048a42c13b7
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-10-07 20:45:53 -0300

    fix(providers): delete the retries on the query provider mutations

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -22,20 +22,6 @@ export const queryClient = new QueryClient({
       },
       refetchOnWindowFocus: true,
     },
-    mutations: {
-      retry: (failureCount, error: unknown) => {
-        if (error && typeof error === "object") {
-          const axiosError = error as AxiosError;
-          const status = axiosError?.response?.status;
-          if (typeof status === "number") {
-            if (status >= 400 && status < 500 && status !== 408) {
-              return false;
-            }
-          }
-        }
-        return failureCount < 2;
-      },
-    },
   },
 });
 
