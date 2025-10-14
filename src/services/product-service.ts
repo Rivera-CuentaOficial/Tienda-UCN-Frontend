18de521cf5ff03fcbb15247fc4e133dd592f6a73
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-10-14 17:20:17 -0300

    feat(services): add admin requests on the product service

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig } from "axios";
 
 import { ApiResponse } from "@/models/generics";
-import { PaginationQueryParams } from "@/models/requests";
+import { CreateProductRequest, PaginationQueryParams } from "@/models/requests";
 import {
   ProductDetailForCustomerResponse,
+  ProductListForAdminResponse,
   ProductListForCustomerResponse,
 } from "@/models/responses";
 
@@ -24,6 +25,29 @@ export class ProductService extends BaseApiService {
   getProductDetail(id: string) {
     return this.httpClient.get<ApiResponse<ProductDetailForCustomerResponse>>(
       `${this.baseURL}/${id}`
+    );
+  }
+
+  getProductsForAdmin(params?: PaginationQueryParams) {
+    return this.httpClient.get<ApiResponse<ProductListForAdminResponse>>(
+      `${this.baseURL}/admin/products`,
+      { params } as AxiosRequestConfig
+    );
+  }
+
+  createProduct(productData: CreateProductRequest) {
+    return this.httpClient.post<ApiResponse<string>>(
+      `${this.baseURL}`,
+      productData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+  }
+
+  toggleProductAvailability(id: string) {
+    return this.httpClient.patch<ApiResponse<string>>(
+      `${this.baseURL}/${id}/toggle-active`
     );
   }
 }
